2d64dd4109e6355d5c856d98feed2842fa79d914
19524341_Trần Xuân Thắng <90304079+thang30102001@users.noreply.github.com>
2023-06-16 07:05:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-
-# Sinh vien 2: Tran Xuan Thang - 19524341
+Tran Xuan Thang - 19524341
 
 from flask import Flask, request, redirect, url_for, render_template, send_file
 from sklearn.preprocessing import LabelEncoder
